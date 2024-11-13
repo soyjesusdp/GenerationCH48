e8e80c2521eaf35d580cc1e55d1a508e133dbda5
Jesús Duerto <jesusduertoprado@gmail.com>
2024-11-13 12:46:19 -0600

    Se actualizan las carpetas

--- REACT/space-app/src/components/about.jsx
+++ REACT/space-app/src/components/about.jsx
@@ -1,14 +1,27 @@
+<<<<<<< HEAD
 import img from '../assets/fotodeperfil.jpg';
+=======
+import img from '../assets/jesus.png';
+>>>>>>> 95ed620 (Se actualizan las carpetas)
 
 function About() {
   return (
     <div>
+<<<<<<< HEAD
       <img src={img} alt="Jesús Duerto" className='profile-img' />
       <h1>Jesús Duerto</h1>
       <p className='profile-text'>
       Jesús es Desarrollador Java Full Stack Jr, Diseñador Gráfico especializado en UX Marketing y emprendedor en marketing de redes sociales con su empresa JD Marketing. Está desarrollando habilidades en programación (Java, Python, HTML, CSS, JavaScript) y construyendo un portafolio personal. Además, maneja proyectos de diseño web, colabora en equipos de desarrollo y sigue un plan de aprendizaje de inglés con enfoque profesional. Es un apasionado del manga y le gusta crear contenido visual y educativo en marketing, buscando siempre mejorar sus habilidades técnicas y blandas para impulsar su carrera.
       </p>
       <a className='profile-user' href='https://www.instagram.com/soyjesusdp'>@soyjesusdp</a>
+=======
+      <img src={img} alt="Katya Echazarreta" />
+      <h2>Katya Echazarreta</h2>
+      <span>@katvoltage</span>
+      <p>
+        Katya Celeste Echazarreta González, conocida como Katya Echazarreta, es una ingeniera electrónica y divulgadora científica mexicana. En junio de 2022 se convirtió en la primera mujer mexicana en viajar al espacio exterior como parte de la misión Blue Origin NS-21 a bordo de una nave espacial New Shepard.
+      </p>
+>>>>>>> 95ed620 (Se actualizan las carpetas)
     </div>
   );
 }
