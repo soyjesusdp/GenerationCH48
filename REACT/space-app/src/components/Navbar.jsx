8f0f6c553053dcda54ce7fffcecd3b04134ab43b
Jesús Duerto <jesusduertoprado@gmail.com>
2024-11-13 12:46:19 -0600

    Se actualizan las carpetas

---
+++ REACT/space-app/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import './navbar.css';
+import { Link } from 'react-router-dom';
+
+function NavButton({ href, text }) {
+  return (
+    <Link className="btn-nav" to={href}>
+      {text}
+    </Link>
+  );
+}
+
+function Navbar({ links }) {
+  console.log(links);
+  const title = "Space App";
+
+  return (
+    <nav>
+      <div>
+        <h1>{title}</h1>
+      </div>
+      <div>
+        {links.map((link, id) => (
+          <NavButton href={link.href} text={link.text} key={id} />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
