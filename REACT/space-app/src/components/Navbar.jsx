c706845e0ea67cf89fbd7be6b68b010d474041d2
Jesús Duerto <jesusduertoprado@gmail.com>
2024-11-15 08:40:39 -0600

    Bug fixes

--- REACT/space-app/src/components/Navbar.jsx
+++ REACT/space-app/src/components/Navbar.jsx
@@ -1,24 +1,31 @@
+// Importamos el archivo de estilos y la herramienta Link para la navegación.
 import './navbar.css';
 import { Link } from 'react-router-dom';
 
+// Componente reutilizable para cada botón de navegación.
 function NavButton({ href, text }) {
+  // Devuelve un enlace estilizado con los props de destino y texto.
   return (
-    <Link className="btn-nav" to={href}>
+    <Link className="nav-button" to={href}>
       {text}
     </Link>
   );
 }
 
+// Componente principal de la barra de navegación.
 function Navbar({ links }) {
-  console.log(links);
-  const title = "Space App";
+  // Definimos un título estático para el navbar.
+  const title = "Jesús Duerto";
 
   return (
-    <nav>
-      <div>
+    <nav className="navbar">
+      {/* Contenedor del título */}
+      <div className="navbar-title">
         <h1>{title}</h1>
       </div>
-      <div>
+      {/* Contenedor de los enlaces de navegación */}
+      <div className="navbar-links">
+        {/* Iteramos sobre los enlaces recibidos por props */}
         {links.map((link, id) => (
           <NavButton href={link.href} text={link.text} key={id} />
         ))}
--- REACT/space-app/src/components/about.jsx
+++ REACT/space-app/src/components/about.jsx
@@ -1,14 +1,14 @@
-import img from '../assets/jesus.png';
+import img from '../assets/fotodeperfilcuadrada.png';
 
 function About() {
   return (
     <div>
-      <img src={img} alt="Katya Echazarreta" />
-      <h2>Katya Echazarreta</h2>
-      <span>@katvoltage</span>
-      <p>
-        Katya Celeste Echazarreta González, conocida como Katya Echazarreta, es una ingeniera electrónica y divulgadora científica mexicana. En junio de 2022 se convirtió en la primera mujer mexicana en viajar al espacio exterior como parte de la misión Blue Origin NS-21 a bordo de una nave espacial New Shepard.
-      </p>
+      <img src={img} alt="Foto de perfil Jesús Duerto" className='about-profilepicture' />
+      <h2>Jesús Duerto</h2>
+      <p className='about-textprofile'>Jesús es Desarrollador Java Full Stack Jr, Diseñador Gráfico especializado en UX Marketing y emprendedor en marketing de redes sociales con su empresa JD Marketing. Está desarrollando habilidades en programación (Java, Python, HTML, CSS, JavaScript) y construyendo un portafolio personal. Además, maneja proyectos de diseño web, colabora en equipos de desarrollo y sigue un plan de aprendizaje de inglés con enfoque profesional. Es un apasionado del manga y le gusta crear contenido visual y educativo en marketing, buscando siempre mejorar sus habilidades técnicas y blandas para impulsar su carrera.</p>
+      <div className='about-instagramprofile-center'>
+        <span className='about-instagramprofile'>@soyjesusdp</span>
+      </div>
     </div>
   );
 }
